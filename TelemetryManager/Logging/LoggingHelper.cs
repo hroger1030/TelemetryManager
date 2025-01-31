3b858a761ed469473b5b7e79cc3199254f6c9edc
roger.hill <hroger1030@gmail.com>
2025-01-30 20:12:10 -0800

    added helper file to map string to enum values

---
+++ TelemetryManager/Logging/LoggingHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TelemetryManager
+{
+    public static class LoggingHelper
+    {
+        public static LoggingLevel LoggingLevelStringMapper(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException(nameof(input));
+
+            switch (input.ToLower().Trim())
+            {
+                case "error": return LoggingLevel.Error;
+                case "warn": return LoggingLevel.Warn;
+                case "info": return LoggingLevel.Info;
+                case "debug": return LoggingLevel.Debug;
+                case "fatal": return LoggingLevel.Fatal;
+
+                default: throw new ArgumentException($"Unable to parse '{input}' into a known LoggingLevel");
+            }
+        }
+    }
+}
